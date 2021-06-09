10062b28e4034b0db2ba8cc140426a141b875655
mkabbe <mukund.kabbe@ki.se>
2021-06-09 14:28:32 +0200

    updated for building seurat object

--- src/signac_build_merged_peak_object.R
+++ src/signac_build_merged_peak_object.R
@@ -1,4 +1,3 @@
-devtools::install_github("timoast/signac", ref = "develop")
 library(Signac)
 library(Seurat)
 library(dplyr)
@@ -8,47 +7,42 @@ library(ggplot2)
 library(patchwork)
 library(rtracklayer)
 
-setwd("//Volumes/shared/Molekylär Neurobiologi/Castelo-Branco/Mukund/CZI_ADULT/CZI_ATAC/")
-getwd()
+setwd("/Volumes/projects/Castelo_Branco/Mukund/CZI_ADULT/CZI_ATAC/data/AGG_ATAC_210507")
 
-
-DATA_PATH <- "../../../NGSDATA/scATAC_human_adult_CZI_1/Processed_data/AGG_ATAC_210218/outs/"
-FRAGMENTS_PATH <- paste0(DATA_PATH,"fragments.tsv.gz")
-CELLS <- read.csv("data/epi/LSI-2_barcodes.txt", header=F)
-FEATURES_PATH <- "data/epi/merged_louvain_cluster_peaks.bed"
-FEATURES <- rtracklayer::import(FEATURES_PATH)
-FRAGMENTS <- CreateFragmentObject(FRAGMENTS_PATH)
+FRAGMENTS_path <- "outs/fragments.tsv.gz"
+CELLS <- read.csv("outs/barcodes_for_LSI-2.csv", header=F)
+FEATURES_bed <- "outs/210510_merged_louvain_cluster_peaks.bed"
+FEATURES <- rtracklayer::import(FEATURES_bed)
 
 cell_bc = c()
 for (i in CELLS) {
   cell_bc <- append(cell_bc, i)
 }
 
+FRAGMENTS <- CreateFragmentObject(FRAGMENTS_path, cells= cell_bc)
+
 count.matrix.peaks <- FeatureMatrix(fragments = FRAGMENTS,
                                     features = FEATURES,
                                     cells = cell_bc)
+#library(Matrix)
+#writeMM(count.matrix.peaks, file = "count_peak_matrix.mtx")
 
 chrom_assay <- CreateChromatinAssay(
   counts = count.matrix.peaks,
   sep = c(":","-"),
   genome = "hg38",
-  fragments = FRAGMENTS_PATH,
+  fragments = FRAGMENTS,
   min.cells = 1,
   min.features = 10
 )
 
-adata.obs <- read.csv(file = "data/epi/LSI-2_RESULTS_metadata.csv")
-rownames(adata.obs) <- adata.obs$X
-adata.obs <- subset(adata.obs, select = -X)
+adata.obs <- read.csv(file = "outs/metadata_for_LSI2.csv")
+obs_df <- data.frame(row.names = "X", adata.obs)
 
 czi_atac <- CreateSeuratObject(
   counts = chrom_assay,
   assay = "peaks",
-  meta.data = adata.obs
+  meta.data = obs_df
 )
 
-saveRDS(czi_atac, file = "data/signac/seurat_objects/210303_merged_peak_matrix.rds")
-
-
-
-
+saveRDS(czi_atac, file = "signac_out/210609_lsi2_merged_peak_matrix.rds")
